98d83c6fd4bac2e66450485b3cf4451418d44e3d
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-11 14:12:24 -0800

    notdoing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,34 +13,74 @@ env.key_filename = 'my_ssh_private_key.txt'
 
 
 def do_deploy(archive_path):
-    """Deploys archive to webservers"""
-    if os.path.exists(archive_path):
-        try:
-
-            # Extract the filename without extension
-            fileName = os.path.splitext(os.path.basename(archive_path))[0]
-
-            # Upload the archive to /tmp/ directory on the web server
-            put(archive_path, "/tmp/{}.tgz".format(fileName))
-
-            # Create destination directory
-            destination = "/data/web_static/releases/{}".format(fileName)
-            run("mkdir -p {}".format(destination))
-
-            # Extract the archive to the destination directory
-            run("tar -xzf /tmp/{}.tgz -C {}".format(fileName, destination))
-
-            # Remove the archive from /tmp/
-            run("rm -rf /tmp/{}.tgz".format(fileName))
-
-            # Remove the symbolic link /data/web_static/current
-            run("rm -rf /data/web_static/current")
-
-            # Create a new symbolic link /data/web_static/current
-            run("ln -s {}/ /data/web_static/current".format(destination))
-
-            return True
-        except Exception as e:
-            return False
-    else:
+    """
+    Distributes an archive to your web servers
+    """
+    if not os.path.exists(archive_path):
         return False
+
+    try:
+        archive_filename = os.path.basename(archive_path)
+        archive_no_ext = os.path.splitext(archive_filename)[0]
+
+        """ Upload the archive to /tmp/ directory on the web server """
+        put(archive_path, '/tmp/')
+
+        """ Create the folder for the new version """
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
+
+        """  Uncompress the archive to the folder """
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            archive_filename, archive_no_ext))
+
+        """ Delete the archive from the web server """
+        run('rm /tmp/{}'.format(archive_filename))
+
+        """ Move the contents to the proper directory """
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'
+            .format(archive_no_ext, archive_no_ext))
+
+        """ Delete the unnecessary folder """
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_no_ext))
+
+        """ Delete the symbolic link /data/web_static/current """
+        run('rm -rf /data/web_static/current')
+
+        """ Create a new symbolic link """
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_no_ext))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        return False
+
+
+def deploy():
+    """
+    Main deployment function
+    """
+    """ Call do_pack to create the archive """
+    archive_path = do_pack()
+
+    """ Check if the archive was created successfully """
+    if not archive_path:
+        print("Failed to create the archive. Deployment aborted.")
+        return False
+
+    """ Call do_deploy to distribute the archive """
+    deployment_result = do_deploy(archive_path)
+
+    """ Check the result of the deployment """
+    if deployment_result:
+        print("Deployment successful!")
+        return True
+    else:
+        print("Deployment failed.")
+        return False
+
+
+if __name__ == "__main__":
+    deploy()
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,27 +12,15 @@ env.user = 'ubuntu'
 env.key_filename = 'my_ssh_private_key.txt'
 
 
-
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-    """
+    """create .tgz archive of web_static/ folder"""
     try:
-        """ Create the version folder if it doesn't exist """
-        if not os.path.exists("versions"):
-            local("mkdir -p versions")
-
-        """ Create the archive filename """
-        now = datetime.now()
-        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second)
-
-        """ Compress the contents of the web_static folder """
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-
-        """ Return the archive path if generated successfully """
-        return "versions/{}".format(archive_name)
-
+        arch_name = "web_static_{}.tgz".format(
+            datetime.now().strftime('%Y%m%d%H%M%S'))
+        local("mkdir -p versions/")
+        local("tar -cvzf versions/{} -C {} .".format(
+            arch_name, "web_static/"))
+        return "versions/{}".format(arch_name)
     except Exception as e:
         return None
 
