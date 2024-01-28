c538f17853c4f4731eaa2a7c45b7f27f26236417
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-28 11:36:13 -0800

    done6

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,15 @@
 start a Flask web application
 """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!' on the main route."""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
